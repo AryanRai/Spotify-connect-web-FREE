df89a23b4358ef5ffa0841e771ee59319fc6f654
AryanRai <31175254+AryanRai@users.noreply.github.com>
2021-02-12 15:58:15 +0530

    eeles

---
+++ gui.py
@@ -0,0 +1,3 @@
+import eel
+eel.init('eel')
+eel.start('spotify_web_sdk.html')
--- tkinter.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *  
-root = Tk()  
-# Code to add widget will go here……..  
-w = Label(root, width = "40", height = "15")  
-w.pack()  
-root.mainloop()  
